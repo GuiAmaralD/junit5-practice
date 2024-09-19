d0c5613a6099e3ce674a76605875032cd199c31e
Guilherme Amaral <guilherme-amaral-dias@hotmail.com>
2024-09-19 18:49:47 -0300

    Created all unit tests for subtraction method

--- src/test/java/com/example/junit_study/math/SimpleMathTest.java
+++ src/test/java/com/example/junit_study/math/SimpleMathTest.java
@@ -18,24 +18,24 @@ public class SimpleMathTest {
     @Test
     void testAdditionTwoPositiveNumbers(){
 
-        double expected = 6.2D;
-        double actual = math.addition(6.2D, 0);
+        double expected = 6.2;
+        double actual = math.addition(6.2, 0);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void testAdditionTwoNegativeNumbers(){
-        double expected = -4D;
-        double actual = math.addition(-1D, -3D);
+        double expected = -4;
+        double actual = math.addition(-1, -3);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void testAdditionNegativeAndPositiveNumbers(){
-        double expected = 7D;
-        double actual = math.addition(-9D, 16D);
+        double expected = 7;
+        double actual = math.addition(-9, 16);
 
         assertEquals(expected, actual);
     }
@@ -46,5 +46,47 @@ public class SimpleMathTest {
         assertEquals(Double.POSITIVE_INFINITY, actual, 0.001);
     }
 
-    
+
+
+
+    @Test
+    void testSubtractionTwoPositiveNumbers() {
+        double expected = 6.2;
+        double actual = math.subtraction(8.2, 2.0);
+
+        assertEquals(expected, actual, 0.00000001);
+    }
+
+    @Test
+    void testSubtractionTwoNegativeNumbers() {
+        double expected = 2D;
+        double actual = math.subtraction(-1, -3);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubtractionNegativeMinusPositive() {
+        double expected = -25D;
+        double actual = math.subtraction(-9, 16);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubtractionPositiveMinusNegative() {
+        double expected = 25;
+        double actual = math.subtraction(16, -9);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubtractionWithInfinity() {
+        double actual = math.subtraction(Double.POSITIVE_INFINITY, 1.0);
+        assertEquals(Double.POSITIVE_INFINITY, actual, 0.001);
+    }
 }
+
+
+
