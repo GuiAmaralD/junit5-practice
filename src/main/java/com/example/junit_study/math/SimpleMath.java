e8ecebfc8e3a3fdd4fc2366b89b89161b45f874e
Guilherme Amaral <guilherme-amaral-dias@hotmail.com>
2024-09-19 18:38:24 -0300

    Created all unit tests for addition method

--- src/main/java/com/example/junit_study/math/SimpleMath.java
+++ src/main/java/com/example/junit_study/math/SimpleMath.java
@@ -2,27 +2,27 @@ package com.example.junit_study.math;
 
 public class SimpleMath {
 
-    public Double subtraction(Double n1, Double n2){
+    public double subtraction(double n1, double n2){
         return n1 - n2;
     }
 
-    public Double addition(Double n1, Double n2){
+    public double addition(double n1, double n2){
         return n1 + n2;
     }
 
-    public Double multiplication(Double n1, Double n2){
+    public double multiplication(double n1, double n2){
         return n1 * n2;
     }
 
-    public Double division(Double n1, Double n2){
+    public double division(double n1, double n2){
         return n1/n2;
     }
 
-    public Double mean(Double n1, Double n2){
+    public double mean(double n1, double n2){
         return (n1 + n2)/2;
     }
 
-    public Double squareRoot(Double n){
+    public double squareRoot(double n){
         return Math.sqrt(n);
     }
 }
--- src/test/java/com/example/junit_study/math/SimpleMathTest.java
+++ src/test/java/com/example/junit_study/math/SimpleMathTest.java
@@ -1,17 +1,50 @@
 package com.example.junit_study.math;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleMathTest {
 
+    private SimpleMath math;
+
+    @BeforeEach
+    void setUp() {
+        math = new SimpleMath();
+    }
+
+
     @Test
-    void test(){
-        SimpleMath math = new SimpleMath();
-        Double expected = 8.2D;
-        Double actual = math.addition(6.2D, 2D);
+    void testAdditionTwoPositiveNumbers(){
+
+        double expected = 6.2D;
+        double actual = math.addition(6.2D, 0);
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void testAdditionTwoNegativeNumbers(){
+        double expected = -4D;
+        double actual = math.addition(-1D, -3D);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAdditionNegativeAndPositiveNumbers(){
+        double expected = 7D;
+        double actual = math.addition(-9D, 16D);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddWithInfinity() {
+        double actual = math.addition(Double.POSITIVE_INFINITY, 1.0);
+        assertEquals(Double.POSITIVE_INFINITY, actual, 0.001);
+    }
+
+    
 }
