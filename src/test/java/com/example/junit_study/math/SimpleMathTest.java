5ab6a0b804e39667a11ce1143f7c861614e83c72
Guilherme Amaral <guilherme-amaral-dias@hotmail.com>
2024-09-23 12:19:12 -0300

    Created parameterized and repeated test

--- src/test/java/com/example/junit_study/math/SimpleMathTest.java
+++ src/test/java/com/example/junit_study/math/SimpleMathTest.java
@@ -1,7 +1,6 @@
 package com.example.junit_study.math;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
---
+++ src/test/java/general/ParameterizedTests.java
@@ -0,0 +1,86 @@
+package general;
+
+import com.example.junit_study.math.SimpleMath;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.util.StringUtils;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class ParameterizedTests {
+
+
+    //value source
+    @ParameterizedTest
+    @ValueSource(strings = {"radars are useful", "testing parameterized test", "i am studying", "white spaced"})
+    void containsWhiteSpaceTest(String word){
+        assertTrue(StringUtils.containsWhitespace(word));
+    }
+
+
+    //method source
+    @ParameterizedTest
+    @MethodSource("testParameterizedWithMethodSource")
+    void testDivision(double n1, double n2, double expected){
+        SimpleMath math = new SimpleMath();
+
+        double actual = math.division(n1, n2);
+
+        assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> testParameterizedWithMethodSource(){
+        return Stream.of(
+                arguments(4.2, 1, 4.2),
+                arguments(4, 2, 2),
+                arguments(-8, 4, -2),
+                arguments(-9, 3, -3)
+        );
+    }
+
+    //csv source
+    //este teste irá falhar pois a fruit numero 1 está definida como null
+    //por conta do atributo nullvalues que indica que a strig null será tratada como null de fato
+    @ParameterizedTest
+    @CsvSource(value = {
+            "null, 1",
+            "banana, 2",
+            "orange, 3"
+    }, nullValues = "null")
+    void testWithCsvSource(String fruit, int rank) {
+        assertAll(
+                () -> assertNull(fruit),
+                () -> assertTrue(rank > 0)
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "apple, 1",
+            "banana, 2",
+            "orange, 3"
+    })
+    void testWithCsvSource2(String fruit, int rank) {
+        assertAll(
+                () -> assertNotNull(fruit),
+                () -> assertTrue(rank > 0)
+        );
+    }
+
+    //repeated test
+    @RepeatedTest(4)
+    void repetitionTest(RepetitionInfo info){
+        System.out.println(info.getCurrentRepetition());
+        System.out.println(info.getFailureCount());
+        assertTrue(true);
+    }
+}
